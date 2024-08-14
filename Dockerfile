a8a25e12c7bd004ee36540744a2f092d20d26e28
oonumahiroshi <honuma596@gmail.com>
2024-08-14 20:03:54 +0900

    wp-cli and other environment configurations have been upgraded.

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM wordpress:latest
+
+COPY ./php.ini /usr/local/etc/php/php.ini
+
+RUN apt update
+
+RUN curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar \
+    && chmod +x wp-cli.phar \
+    && mv wp-cli.phar /usr/local/bin/wp \
+    && wp --info
---
+++ wp-install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# WordPress install
+#=======================
+wp core install \
+--url="http://localhost:8080" \
+--title="myproject" \
+--admin_user='admin' \
+--admin_password='test' \
+--admin_email='info@example.com' \
+--allow-root
+
+# generally setting
+# =======================
+# Catchphrase empty
+wp option update blogdescription --allow-root ''
+# Language "japan"
+wp language core install --allow-root --activate ja
+# Time zone and date/time notation
+wp option update --allow-root timezone_string 'Asia/Tokyo'
+wp option update --allow-root date_format 'Y-m-d'
+wp option update --allow-root time_format 'H:i'
+
+# Permalink settings
+# =======================
+wp option update permalink_structure --allow-root /%postname%/
+
+# Exterior
+# =======================
+wp theme delete --allow-root twentysixteen
+wp theme delete --allow-root twentyseventeen
+wp theme delete --allow-root twentynineteen
+wp theme delete --allow-root twentytwenty
+
+# Plugins
+# =======================
+wp plugin delete --allow-root hello.php
+wp plugin delete --allow-root akismet
+wp plugin install --allow-root wp-multibyte-patch --activate
